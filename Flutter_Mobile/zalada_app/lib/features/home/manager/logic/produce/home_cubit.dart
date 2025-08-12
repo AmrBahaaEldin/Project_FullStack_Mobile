ad2564f7cf11b28f9db407b07102d33f5d41be06
Amr_BahaaEldin <108246015+AmrBahaaEldin@users.noreply.github.com>
2025-08-12 06:15:34 +0300

    home view
    improve Home View in Logic Cubit

--- Flutter_Mobile/zalada_app/lib/features/home/manager/logic/produce/home_cubit.dart
+++ Flutter_Mobile/zalada_app/lib/features/home/manager/logic/produce/home_cubit.dart
@@ -1,5 +1,4 @@
 import 'package:bloc/bloc.dart';
-import 'package:zalada_app/features/home/data/model/hotdeal/home_hot_deal_data.dart';
 import 'package:zalada_app/features/home/data/repo/repo_home_impl.dart';
 
 import 'home_state.dart';
@@ -7,16 +6,6 @@ import 'home_state.dart';
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit(this.repoHomeIm) : super(HomeInitial());
   final RepoHomeIm repoHomeIm;
-  int currentIndex = 0;
-  void changePage(int index) {
-    if (index == HomeHotDealData.data.length) {
-      currentIndex = 0;
-    } else {
-      currentIndex = index;
-    }
-    emit(HomeChangeBanner());
-  }
-
   void fetchProduceCategory(String categoryProduce) async {
     emit(HomeLoading());
     final response = await repoHomeIm.fetchProduces(
--- Flutter_Mobile/zalada_app/lib/features/navigation/data/model/body_view_model.dart
+++ Flutter_Mobile/zalada_app/lib/features/navigation/data/model/body_view_model.dart
@@ -2,6 +2,9 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zalada_app/core/utils/api_service.dart';
+import 'package:zalada_app/features/home/data/repo/repo_home_impl.dart';
+import 'package:zalada_app/features/home/manager/logic/produce/home_cubit.dart';
+import 'package:zalada_app/features/home/manager/logic/slider_banner/cubit/slider_banner_cubit.dart';
 import 'package:zalada_app/features/home/presentation/view/home_body_view.dart';
 import 'package:zalada_app/features/profile/presentation/view/profile_body_view.dart';
 import 'package:zalada_app/features/search/data/repo/search_repo_impl.dart';
@@ -11,13 +14,22 @@ import 'package:zalada_app/features/search/presentation/view/search_body_view.da
 class BodyViewModel {
   BodyViewModel._();
   static List<Widget> boyWidgets = [
-    const HomeBodyView(),
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => HomeCubit(RepoHomeIm(ApiService(Dio())))..fetchProduceCategory("tops"),
+        ),
+        BlocProvider(
+          create: (context) => SliderBannerCubit(),
+        ),
+      ],
+      child: const HomeBodyView()),
     BlocProvider(
       create: (context) => SearchCubit(SearchRepoImpl(ApiService(Dio()))),
       child: const SearchBodyView(),
     ),
     const Center(child: Text('❤️ Favorites', style: TextStyle(fontSize: 24))),
     const Center(child: Text('❤️ Cart', style: TextStyle(fontSize: 24))),
-    const ProfileBodyView(),
+    const  ProfileBodyView(),
   ];
 }
